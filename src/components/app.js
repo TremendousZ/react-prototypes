151bdb1d850df461e6717fb45cf77b502321be5e
John Carlisle <jmcarlisle@live.com>
2018-08-07 20:44:04 -0700

    feature set 1 complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,11 @@
+import React from'react';
+import './app.css';
 
-
+function App(){
+    return (
+        <div className='main-container'>
+            <h1 className='main-title'>Scrapbook</h1>
+        </div>
+    )
+}
+export default App;
