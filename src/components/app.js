9437b19920d44cea43c8bcf42710c9cf1adf8200
John Carlisle <jmcarlisle@live.com>
2018-08-07 21:05:34 -0700

    feature set 2 complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from'react';
 import './app.css';
+import Scrapbook from './scrapbook';
 
 function App(){
     return (
         <div className='main-container'>
             <h1 className='main-title'>Scrapbook</h1>
+            <Scrapbook />
         </div>
     )
 }
---
+++ src/components/scrapbook.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './scrapbook.css';
+import Vader from './images/vaderyou.jpg';
+
+export default()=>{
+    return (
+        <div className ='scrapbook-container'>
+            <img src={Vader}/>
+        </div>
+    )
+
+}
