793d42531cc840c60b84f198c7ff9ee4ac8b564d
John Carlisle <jmcarlisle@live.com>
2018-08-17 23:39:12 -0700

    feature set 2 complete

---
+++ src/reducers/clock_reducer.js
@@ -0,0 +1,12 @@
+
+
+const DEFAULT_STATE = {
+    time:new Date().toLocaleTimeString()
+}
+
+export default (state=DEFAULT_STATE, action)=>{
+    switch(action.type){
+        default:
+            return state;
+    }
+}
